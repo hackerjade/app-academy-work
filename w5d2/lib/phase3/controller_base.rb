e6b43c4129f246fafce2c8c96183e5b91725707f
Jade McPherson <mcpherson.tahnij@gmail.com>
2015-05-19 17:39:05 -0700

    Rails lite day 1

---
+++ w5d2/lib/phase3/controller_base.rb
@@ -0,0 +1,21 @@
+require_relative '../phase2/controller_base'
+require 'active_support'
+require 'active_support/inflector'
+require 'active_support/core_ext'
+require 'erb'
+
+module Phase3
+  class ControllerBase < Phase2::ControllerBase
+    # use ERB and binding to evaluate templates
+    # pass the rendered html to render_content
+    def render(template_name)
+      controller_name = self.class.to_s.underscore
+      template_path = "views/#{controller_name}/#{template_name}.html.erb"
+      template = File.read(template_path)
+
+      template_content = ERB.new(template).result(binding)
+
+      render_content(template_content, "text/html")
+    end
+  end
+end
