aac306a33f96f33f7e357c18057d99bde8215ed0
Jade McPherson <mcpherson.tahnij@gmail.com>
2015-05-22 18:35:02 -0700

    Working Towers of Hanoi

--- w5d5/towers.js
+++ w5d5/towers.js
@@ -14,7 +14,6 @@ TowersOfHanoi.prototype.isWon = function() {
   return this.stacks[1].length === 3 || this.stacks[2].length === 3;
 };
 
-
 TowersOfHanoi.prototype.isValidMove = function(startIdx, endIdx) {
 
   if (startIdx < 0 || startIdx > 2 || endIdx < 0 || endIdx > 2) {
@@ -39,30 +38,30 @@ TowersOfHanoi.prototype.move = function(startIdx, endIdx) {
 };
 
 TowersOfHanoi.prototype.print = function() {
+  console.log("\n");
   console.log(this.stacks);
+  console.log("\n");
 };
 
 TowersOfHanoi.prototype.promptMove = function(callback) {
   this.print();
   reader.question("Where do you want to move?", function (answer) {
     callback(answer[0], answer[2]);
-  });
+  }.bind(this));
 };
 
 TowersOfHanoi.prototype.run = function(callback) {
-
-  this.promptMove(function(startIdx, endIdx) {
-    if (this.move(startIdx, endIdx)) {
-      this.run(callback);
-    } else {
+  var that = this
+  function promptCallback(startIdx, endIdx) {
+    if (!that.move(startIdx, endIdx)) {
       console.log("Error son");
-      this.run(callback);
     }
-  });
 
-  if (this.isWon()) {
-    callback();
+    that.isWon() ? callback() : that.run(callback);
   }
+
+
+  this.promptMove(promptCallback);
 };
 
 var game = new TowersOfHanoi();
